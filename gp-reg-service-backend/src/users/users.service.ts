a07714da5f8293ccf390dd13f220823da1fc1851
kavin-du <ukdkcm@gmail.com>
2023-04-10 07:21:24 +0530

    crud for users

---
+++ gp-reg-service-backend/src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const newuser = this.userRepository.create({
+      ...createUserDto,
+      createdAt: new Date(),
+    });
+    return this.userRepository.save(newuser);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(id, { ...updateUserDto });
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
